e89cb6086aa0618735ad72e0cd4104596b3da9b4
Andrei Gorevoi <18.andreigorevoi@gmail.com>
2020-04-15 17:44:25 +0300

    News entity has been added;

--- news_projects/src/main/java/com/news/entity/News.java
+++ news_projects/src/main/java/com/news/entity/News.java
@@ -1,4 +1,36 @@
 package com.news.entity;
 
 public class News {
+    private int id;
+    private String title;
+    private double rate;
+
+    public News(String title, double rate) {
+        this.title = title;
+        this.rate = rate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 }
